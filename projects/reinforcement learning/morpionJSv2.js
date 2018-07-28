58dfc3165790b1c23ab327615b8afbd9b94de17f
Sylvain Sénéchal <senechalsylvain@hotmail.com>
2018-07-28 07:33:52 +0200

    v2Tictactoe and text prediction

--- projects/reinforcement learning/morpionJSv2.js
+++ projects/reinforcement learning/morpionJSv2.js
@@ -19,8 +19,10 @@ function init(){
 
 function loop(){ // Voir l'ordre des fonctions
   dessin()
-  allowingPlay()
-  resultGame()
+  for(let speed = 0; speed<50; speed++){
+    allowingPlay()
+    resultGame()
+  }
   requestAnimationFrame(loop);
 }
 
@@ -52,16 +54,27 @@ function restoreGame(){
     jeu.j2Won = 0 // total won by random Player
     jeu.noWin = 0
 
-    let maxFitness = - 10000
-    let iFitness = 0
-    for(i=0; i<jeu.nbGames; i++){
-      if(jeu.listFitness[i] > maxFitness){
-        iFitness = i
-        maxFitness = jeu.listFitness[i]
-      }
+    // let maxFitness = - 10000
+    // let iFitness = 0
+    // for(i=0; i<jeu.nbGames; i++){
+    //   if(jeu.listFitness[i] > maxFitness){
+    //     iFitness = i
+    //     maxFitness = jeu.listFitness[i]
+    //   }
+    // }
+    // saveBrain(iFitness)
+    // console.log(JSON.stringify(savedBrain.brain))
+
+    // reduction des ecarts :
+    let minFit = Math.min(...jeu.listFitness)
+    jeu.listFitness = jeu.listFitness.map(elem => elem -= minFit)
+    // On commence par mettre a la puissance 4 les fitness pour garder vraiment les meilleures
+    for(let a=0; a<jeu.listGame.length; a++){
+      jeu.listFitness[a] = Math.pow(jeu.listFitness[a], 4)
+      //jeu.listFitness[a] = Math.pow(1.05, jeu.listFitness[a])
+      //console.log(jeu.listFitness[a])
     }
-    saveBrain(iFitness)
-    //console.log(JSON.stringify(savedBrain.brain))
+    calculateProbability()
     for(a=0; a<jeu.nbGames; a++){
       jeu.listGame[a] = new SingleGame("similarBrain")
       jeu.listWon[a] = 0
@@ -98,6 +111,7 @@ function printData(){
   console.log("Max lost: " + Math.max(...jeu.listLost))
   console.log("Max null: " + Math.max(...jeu.listNoWon))
   console.log("Fitness Max : " + Math.max(...jeu.listFitness))
+  console.log('fitness min : ' + Math.min(...jeu.listFitness))
   let cpt = 0
   for(i=0; i<jeu.nbGames; i++){
     cpt += jeu.listFitness[i]
@@ -111,11 +125,11 @@ function printData(){
 }
 
 var jeu = {
-  widthCase: 11, // 11 idéal
+  widthCase: 11,
   nbGamesXaxis: 20, // 20 idéal
-  nbGamesYaxis: 20,
+  nbGamesYaxis: 20, // 20 idéal
   grid: [],
-  nbGames: 400, // nbGamesXaxis * nbGamesYaxis
+  nbGames: 800, // nbGamesXaxis * nbGamesYaxis
   listGame: [],
   listWon: [],
   listNoWon: [],
@@ -125,8 +139,6 @@ var jeu = {
 	positionX: 50,
 	positionY: 50,
 
-  lastTimePlayed: 0,
-  delay: 1, // useless < 15 -> 60 fps
   j1Won: 0, // total won by IA
   j2Won: 0, // total won by random Player
   noWin: 0, // total null match
@@ -136,7 +148,8 @@ var jeu = {
   score: 0,
   nbGeneration: 1,
   maxFitness: 0,
-  gamesBeforeNewGen: 20,
+  gamesBeforeNewGen: 100,
+  mutationRate: 0.001
 }
 
 var savedBrain = {
@@ -153,53 +166,86 @@ SingleGame = function(similarBrain, sameBrain){
   this.grid = [],
   this.nextPlayer = Math.floor(1+Math.random()*2),
   this.fitness = 0
+  this.probabilite = 0
   this.lastTimePlayed = 0
   this.ended = false
   this.brain = []
-  if(arguments.length == 0){
+  if(arguments.length == 0){ // Initialisation tout debut
     for(i=0; i<9; i++){
-        this.brain[i] = new Brain() // Chaque brain est rattaché à l'activation d'une case, je séléctionne la case la plus activée...
+      this.brain[i] = new Brain() // Chaque brain est rattaché à l'activation d'une case, je séléctionne la case la plus activée...
     }
   }
-  else if(arguments.length == 1){
-    for(i=0; i<9; i++){
-        this.brain[i] = new Brain(i)
+  else if(arguments.length == 1){ // Nouveau brain après 20 partie
+    // Selection de 2 parents
+    let mother = pickParent()
+    let father = pickParent()
+
+    // CrossOver de ces 2 parents pour  donner un fils
+    let randomCross = Math.floor(Math.random()*mother.brain.length)
+    for(m=0; m<randomCross; m++){
+      this.brain[m] = mother.brain[m]
+    }
+    for(n=randomCross; n<mother.brain.length; n++){
+      this.brain[n] = father.brain[n]
+    }
+    // Mutations potentielles du fils
+    for(let i=0; i<this.brain.length; i++){
+      //for(let j=0; j<this.brain[0].cells.length; j++){
+        let rd = Math.random()
+        if(rd < jeu.mutationRate){
+          //this.brain[i].cells[j] += -1 + Math.random()*2
+          this.brain[i] = new Brain()
+        }
+      //}
     }
   }
-  else if(arguments.length == 2){
+  else if(arguments.length == 2){ // brain identique pendant les 20 parties
     this.brain = sameBrain
   }
 }
 
-Brain = function(iBrain){
-  if(arguments.length == 0){
-    this.w1 = -1 + Math.random()*2
-    this.w2 = -1 + Math.random()*2
-    this.w3 = -1 + Math.random()*2
-    this.w4 = -1 + Math.random()*2
-    this.w5 = -1 + Math.random()*2
-    this.w6 = -1 + Math.random()*2
-    this.w7 = -1 + Math.random()*2
-    this.w8 = -1 + Math.random()*2
-    this.w9 = -1 + Math.random()*2
-    this.w10 = -1 + Math.random()*2
+var copyListFitness = []
+var copyListGames = []
+function calculateProbability(){
+  for(let k=0; k<jeu.listGame.length; k++){
+    copyListFitness[k] = jeu.listFitness[k]
+    copyListGames[k] = jeu.listGame[k]
   }
-  else if(arguments.length == 1){
-    this.w1 = savedBrain.brain[iBrain].w1 + (-1 + Math.random()*2)/3
-    this.w2 = savedBrain.brain[iBrain].w2 + (-1 + Math.random()*2)/3
-    this.w3 = savedBrain.brain[iBrain].w3 + (-1 + Math.random()*2)/3
-    this.w4 = savedBrain.brain[iBrain].w4 + (-1 + Math.random()*2)/3
-    this.w5 = savedBrain.brain[iBrain].w5 + (-1 + Math.random()*2)/3
-    this.w6 = savedBrain.brain[iBrain].w6 + (-1 + Math.random()*2)/3
-    this.w7 = savedBrain.brain[iBrain].w7 + (-1 + Math.random()*2)/3
-    this.w8 = savedBrain.brain[iBrain].w8 + (-1 + Math.random()*2)/3
-    this.w9 = savedBrain.brain[iBrain].w9 + (-1 + Math.random()*2)/3
-    this.w10 = savedBrain.brain[iBrain].w10 + (-1 + Math.random()*2)/3
+  let sum = 0
+  for(let a=0; a<jeu.listGame.length; a++){
+    sum += copyListFitness[a]
+  }
+  for(let a=0; a<jeu.listGame.length; a++){
+    copyListGames[a].probabilite = copyListFitness[a] / sum
   }
 }
 
+function pickParent(){
+  let index = 0
+  let rdm = Math.random()
+  while( rdm > 0){
+    rdm = rdm - copyListGames[index].probabilite
+    index++
+  }
+  index--
+  return copyListGames[index]
+}
+
+Brain = function(iBrain){
+  this.cells = []
+  if(arguments.length == 0){
+    for(b=0; b<10; b++){
+      this.cells[b] = -1 + Math.random()*2
+    }
+  }
+  // else if(arguments.length == 1){
+  //   for(i=0; i<10; i++){
+  //     this.cells[i] = savedBrain.brain[iBrain].cells[i] + (-1 + Math.random()*2)/3
+  //   }
+  // }
+}
+
 function useBrain(idGame, brain){
-  // inputs = [xBird, xFirstPipe, yTopPipe, yBottomPipe] + normalize
   let i1 = (jeu.listGame[idGame].grid[0][0]) / 2 // (val = 0||1||2) --> range = 2
   let i2 = (jeu.listGame[idGame].grid[0][1]) / 2 // (val = 0||1||2) --> range = 2
   let i3 = (jeu.listGame[idGame].grid[0][2]) / 2 // (val = 0||1||2) --> range = 2
@@ -210,9 +256,9 @@ function useBrain(idGame, brain){
   let i8 = (jeu.listGame[idGame].grid[2][1]) / 2 // (val = 0||1||2) --> range = 2
   let i9 = (jeu.listGame[idGame].grid[2][2]) / 2 // (val = 0||1||2) --> range = 2
   let bias = 1
-  return i1*brain.w1 + i2*brain.w2 + i3*brain.w3 +
-          i4*brain.w4 + i5*brain.w5 + i6*brain.w6 +
-          i7*brain.w7 + i8*brain.w8 + i9*brain.w9 + bias*brain.w10
+  return i1*brain.cells[0] + i2*brain.cells[1] + i3*brain.cells[2] +
+          i4*brain.cells[3] + i5*brain.cells[4] + i6*brain.cells[5] +
+          i7*brain.cells[6] + i8*brain.cells[7] + i9*brain.cells[8] + bias*brain.cells[9]
 }
 
 
@@ -220,10 +266,10 @@ function useBrain(idGame, brain){
 function allowingPlay(){
   for(a=0; a<jeu.nbGames; a++){
     if(jeu.listGame[a].ended == false){
-      if(jeu.listGame[a].nextPlayer == 1 && jeu.listGame[a].lastTimePlayed + jeu.delay <= performance.now() ){
+      if(jeu.listGame[a].nextPlayer == 1 ){
         playComputer1(a)
       }
-      else if(jeu.listGame[a].nextPlayer == 2 && jeu.listGame[a].lastTimePlayed + jeu.delay <= performance.now() ){
+      else if(jeu.listGame[a].nextPlayer == 2 ){
         playComputer2(a)
       }
     }
@@ -231,7 +277,6 @@ function allowingPlay(){
 }
 function playComputer1(idGame){
   // Ia :
-
   let g = []
   for(i=0; i<3; i++){
     g[i] = []
@@ -265,7 +310,6 @@ function playComputer1(idGame){
   jeu.listGame[idGame].grid[best.valI][best.valJ] = 1
 
   jeu.listGame[idGame].nextPlayer = 2
-  jeu.listGame[idGame].lastTimePlayed = performance.now()
 }
 
 function playComputer2(idGame){
@@ -287,11 +331,12 @@ function playComputer2(idGame){
 
 function calculateFitness(idGame, valCheckWon){ // A revoir
   if(valCheckWon == 0){ // Si match null, fitness = 10
-    jeu.listGame[idGame].fitness = 10
+    jeu.listGame[idGame].fitness = 10 // 10
+    //return 10
     return 10
   }
   if(valCheckWon == 1){ // Si j'ai gagné, je veux gagner avec le moins de coups possibles
-    let fitness = 20
+    let fitness = 30
     let cpt = 9
     for(i=0; i<3; i++){
       for(j=0; j<3; j++){
@@ -303,9 +348,10 @@ function calculateFitness(idGame, valCheckWon){ // A revoir
     fitness += cpt
     jeu.listGame[idGame].fitness = fitness
     return fitness
+    //return 6
   }
   if(valCheckWon == 2){
-    let fitness = -20
+    let fitness = 0
     let cpt = 0
     for(i=0; i<3; i++){
       for(j=0; j<3; j++){
@@ -317,6 +363,7 @@ function calculateFitness(idGame, valCheckWon){ // A revoir
     fitness += cpt // plus on a mis de temps a PERDE plus on augmente la fitness
     jeu.listGame[idGame].fitness = fitness
     return fitness
+    //return 0
   }
 }
 
@@ -405,7 +452,7 @@ function checkWon(idGame){
 function dessin(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
   for(u=0; u<jeu.nbGamesXaxis; u++){
-    for(v=0; v<jeu.nbGamesXaxis; v++){
+    for(v=0; v<jeu.nbGamesYaxis; v++){
       for(i=0; i<3; i++){
         for(j=0; j<3; j++){
           ctx.strokeRect(jeu.positionX+i*jeu.widthCase + u*jeu.widthCase*4, jeu.positionY+j*jeu.widthCase + v*jeu.widthCase*4, jeu.widthCase, jeu.widthCase);
@@ -415,7 +462,7 @@ function dessin(){
   }
 
   for(u=0; u<jeu.nbGamesXaxis; u++){
-    for(v=0; v<jeu.nbGamesXaxis; v++){
+    for(v=0; v<jeu.nbGamesYaxis; v++){
       for(i=0; i<3; i++){
         for(j=0; j<3; j++){
           if(jeu.listGame[(u*10)+v].grid[j][i] == 1){
