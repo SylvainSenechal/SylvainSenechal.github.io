bf02d1e2f3b54d4de8e1dd33ba61f2e93b571d26
Sylvain Sénéchal <senechalsylvain@hotmail.com>
2018-08-27 15:55:31 +0200

    new shader

--- script.js
+++ script.js
@@ -9,11 +9,99 @@ console.log(pj)
 //     console.log('oui')
 //   })
 // })
-pj.addEventListener('mousemove', function(e){
-    console.log('oui')
-  });
+
 // Array.from(pj).forEach(function(element) {
 //   element.addEventListener('mousemove', function(e){
 //     console.log('oui')
 //   });
 // });
+
+
+var uniforms
+var scene, camera, fieldOfView, aspectRatio, height, width, nearPlane, farPlane, renderer, container
+
+const init = () => {
+  createScene()
+  //createLights() // A ajouter
+  createMesh()
+
+  resizeScene() // A appeler dans create scene ?
+  loop()
+}
+
+const loop = () => {
+  render()
+
+  requestAnimationFrame(loop)
+}
+
+const render = () => {
+  uniforms.u_time.value += 60/1000
+  renderer.render( scene, camera )
+}
+
+const createScene = () => {
+  scene = new THREE.Scene()
+
+  aspectRatio = width / height
+	fieldOfView = 60
+	nearPlane = 1
+	farPlane = 40000
+  camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane)
+	camera.rotation.order = 'YXZ' // default is 'XYZ'
+	camera.position.x = 0 // gauche/droite
+	camera.position.z = 1 // profondeur
+	camera.position.y = 0 // hauteur
+
+  renderer = new THREE.WebGLRenderer({ // voir tous les arguments existants
+		alpha: true,
+		antialias: true,
+		shadowMap: THREE.PCFSoftShadowMap
+	});
+  height = window.innerHeight
+	width = window.innerWidth
+	renderer.setSize(width, height)
+	renderer.shadowMap.enabled = true
+
+  container = document.getElementById( 'myContainer' )
+  container.appendChild(renderer.domElement)
+}
+
+const createMesh = () => {
+  var geometry = new THREE.PlaneBufferGeometry(2, 2)
+
+  uniforms = {
+      u_time: { type: "f", value: 1.0 },
+      u_resolution: { type: "v2", value: new THREE.Vector2() },
+      u_mouse: { type: "v2", value: new THREE.Vector2() },
+  };
+
+  var material = new THREE.ShaderMaterial( {
+      uniforms: uniforms,
+      vertexShader: document.getElementById( 'vertexShader' ).textContent,
+      fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+  } );
+
+  var mesh = new THREE.Mesh(geometry, material)
+  scene.add(mesh)
+}
+
+document.onmousemove = function(e){
+  uniforms.u_mouse.value.x = e.x
+  uniforms.u_mouse.value.y = height - e.y
+}
+
+
+const resizeScene = () => {
+  console.log('oui')
+	height = window.innerHeight
+	width = window.innerWidth
+	camera.aspect = width / height
+	camera.updateProjectionMatrix()
+	renderer.setSize(width, height)
+  uniforms.u_resolution.value.x = renderer.domElement.width
+  uniforms.u_resolution.value.y = renderer.domElement.height
+}
+
+window.addEventListener('load', init, false); // voir l'argument false ?
+window.addEventListener('resize', resizeScene, false);
