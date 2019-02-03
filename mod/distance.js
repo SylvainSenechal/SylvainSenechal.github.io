43570a113fe40f49311c50ab27c4f00381a74dc1
Sylvain Sénéchal <senechalsylvain@hotmail.com>
2019-02-03 16:52:10 +0100

    cleanup

--- mod/distance.js
+++
@@ -1,1 +0,0 @@
-export const dst = (a, b) => a + b
--- script.js
+++ script.js
@@ -1,201 +1,95 @@
 'use strict';
-// browser-sync start --server -f -w
 
-// import vertParticles from './mod/fbm.vert'
-import {dst} from './mod/distance.js'
-console.log(dst(5,3))
-THREE.blurShader = {
+/////////////////////////
+// FBM SHADER ///////////
+/////////////////////////
 
-	uniforms: {
-
-		"mRefractionRatio": { value: 1.02 },
-		"mFresnelBias": { value: 0.1 },
-		"mFresnelPower": { value: 2.0 },
-		"mFresnelScale": { value: 1.0 },
-		"tCube": { value: null }
-
-	},
-
-	vertexShader: [
-
-		"uniform float mRefractionRatio;",
-		"uniform float mFresnelBias;",
-		"uniform float mFresnelScale;",
-		"uniform float mFresnelPower;",
-
-		"varying vec3 vReflect;",
-		"varying vec3 vRefract[3];",
-		"varying float vReflectionFactor;",
-
-		"void main() {",
-
-			"vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
-			"vec4 worldPosition = modelMatrix * vec4( position, 1.0 );",
-
-			"vec3 worldNormal = normalize( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal );",
-
-			"vec3 I = worldPosition.xyz - cameraPosition;",
-
-			"vReflect = reflect( I, worldNormal );",
-			"vRefract[0] = refract( normalize( I ), worldNormal, mRefractionRatio );",
-			"vRefract[1] = refract( normalize( I ), worldNormal, mRefractionRatio * 0.99 );",
-			"vRefract[2] = refract( normalize( I ), worldNormal, mRefractionRatio * 0.98 );",
-			"vReflectionFactor = mFresnelBias + mFresnelScale * pow( 1.0 + dot( normalize( I ), worldNormal ), mFresnelPower );",
-
-			"gl_Position = projectionMatrix * mvPosition;",
-
-		"}"
-
-	].join( "\n" ),
-
-	fragmentShader: [
-
-		"uniform samplerCube tCube;",
-
-		"varying vec3 vReflect;",
-		"varying vec3 vRefract[3];",
-		"varying float vReflectionFactor;",
-
-		"void main() {",
-
-			"vec4 reflectedColor = textureCube( tCube, vec3( -vReflect.x, vReflect.yz ) );",
-			"vec4 refractedColor = vec4( 1.0 );",
-
-			"refractedColor.r = textureCube( tCube, vec3( -vRefract[0].x, vRefract[0].yz ) ).r;",
-			"refractedColor.g = textureCube( tCube, vec3( -vRefract[1].x, vRefract[1].yz ) ).g;",
-			"refractedColor.b = textureCube( tCube, vec3( -vRefract[2].x, vRefract[2].yz ) ).b;",
-
-			"gl_FragColor = mix( refractedColor, reflectedColor, clamp( vReflectionFactor, 0.0, 1.0 ) );",
-
-		"}"
-
-	].join( "\n" )
-
-};
-console.log(THREE.FresnelShader)
-let pj = document.getElementsByClassName('projectLink')
-
-Array.from(pj).forEach(function(element){
-  element.addEventListener('mouseover', function(e){
-    document.getElementById("myCanvas").setAttribute("class", "blurred")
-    Array.from(document.getElementsByClassName("projectLink")).forEach( elem => {
-      if(elem != element){
-        elem.setAttribute("class", "projectLink blurred")
-
-      }
-      else{
-        elem.setAttribute("class", "projectLink")
-
-      }
-    })
-  })
-  element.addEventListener('mouseout', function(e){
-    document.getElementById("myCanvas").setAttribute("class", "notBlurred")
-    Array.from(document.getElementsByClassName("projectLink")).forEach( elem => elem.setAttribute("class", "projectLink notBlurred"))
-  });
-})
-
-// Array.from(pj).forEach(function(element) {
-//   element.addEventListener('mousemove', function(e){
-//     console.log('oui')
-//   });
-// });
-
-
-var uniforms
-var scene, camera, fieldOfView, aspectRatio, height, width, nearPlane, farPlane, renderer, container
+var scene, camera, height, width, renderer, container
+var composer, pass
+var targetX = 0
+var targetY = 0
 
 const init = function(){
   createScene()
-  //createLights() // A ajouter
-  createMesh()
-  resizeScene() // A appeler dans create scene ?
+  resizeScene()
   loop()
 }
 
 const loop = function(){
   render()
-  interpolate()
+  interpolateMouse()
   requestAnimationFrame(loop)
 }
 
-const render = function(){
-  uniforms.u_time.value += 60/1000
-  renderer.render( scene, camera )
+const render = () => {
+  pass.uniforms.u_time.value += 60/1000
+	composer.render()
 }
-console.log("bonjour")
-const createScene = function(){
+
+const createScene = () => {
   scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera()
+  renderer = new THREE.WebGLRenderer()
 
-  aspectRatio = width / height
-	fieldOfView = 60
-	nearPlane = 1
-	farPlane = 40000
-  camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane)
-	camera.rotation.order = 'YXZ' // default is 'XYZ'
-	camera.position.x = 0 // gauche/droite
-	camera.position.z = 1 // profondeur
-	camera.position.y = 0 // hauteur
-
-  renderer = new THREE.WebGLRenderer({ // voir tous les arguments existants
-		alpha: true,
-		antialias: true,
-		shadowMap: THREE.PCFSoftShadowMap
-	});
   height = window.innerHeight
 	width = window.innerWidth
 	renderer.setSize(width, height)
-	renderer.shadowMap.enabled = true
-
-  container = document.getElementById( 'myContainer' )
+  container = document.getElementById('myContainer')
   container.appendChild(renderer.domElement)
+
+	composer = new THREE.EffectComposer(renderer)
+	composer.addPass(new THREE.RenderPass(scene, camera));
+	pass = new THREE.ShaderPass( THREE.fbmShader );
+	pass.uniforms.u_resolution.value.x = renderer.domElement.width
+	pass.uniforms.u_resolution.value.y = renderer.domElement.height
+	pass.renderToScreen = true
+	composer.addPass(pass)
+
   document.getElementsByTagName("canvas")[0].setAttribute("id", "myCanvas")
 }
-const createMesh = function(){
-  var geometry = new THREE.PlaneBufferGeometry(2, 2, 4, 4)
-  let seed = Math.random()*100
-  uniforms = {
-      u_time: {type: "f", value: 1.0},
-      u_resolution: {type: "v2", value: new THREE.Vector2()},
-      u_mouse: {type: "v2", value: new THREE.Vector2()},
-      u_octave: {type: "f", value: 6.0},
-      seed: {type: "f", value: seed},
-  };
 
-  var material = new THREE.ShaderMaterial( {
-      uniforms: uniforms,
-      vertexShader: document.getElementById( 'vertexShader' ).textContent,
-      fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-  } );
-
-  var mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
+document.onmousemove = mouse => {
+  targetX = mouse.x
+  targetY = height - mouse.y
+  let normalized = (15-2) * (mouse.x / width) + 2
+  pass.uniforms.u_octave.value = Math.floor(normalized)
+}
+const interpolateMouse = () => {
+  pass.uniforms.u_mouse.value.x += (targetX-pass.uniforms.u_mouse.value.x) * 0.02
+  pass.uniforms.u_mouse.value.y += (targetY-pass.uniforms.u_mouse.value.y) * 0.02
 }
 
-var targetX = 0
-var targetY = 0
-document.onmousemove = function(e){
-  targetX = e.x
-  targetY = height - e.y
-  let normalized = (15-2) * (e.x / width) + 2
-  console.log(Math.floor(normalized))
-  uniforms.u_octave.value = Math.floor(normalized)
-}
-const interpolate = function(){
-  uniforms.u_mouse.value.x += (targetX-uniforms.u_mouse.value.x) * 0.02
-  uniforms.u_mouse.value.y += (targetY-uniforms.u_mouse.value.y) * 0.02
-}
-
-const resizeScene = function(){
-  console.log('oui')
+const resizeScene = () => {
 	height = window.innerHeight
 	width = window.innerWidth
-	camera.aspect = width / height
-	camera.updateProjectionMatrix()
 	renderer.setSize(width, height)
-  uniforms.u_resolution.value.x = renderer.domElement.width
-  uniforms.u_resolution.value.y = renderer.domElement.height
+  pass.uniforms.u_resolution.value.x = renderer.domElement.width
+  pass.uniforms.u_resolution.value.y = renderer.domElement.height
 }
 
-window.addEventListener('load', init, false); // voir l'argument false ?
-window.addEventListener('resize', resizeScene, false);
+/////////////////////////
+// CSS HANDLING /////////
+/////////////////////////
+
+let projects = document.getElementsByClassName('projectLink')
+
+Array.from(projects).forEach( element => {
+  element.addEventListener('mouseover', mouse => {
+    document.getElementById("myCanvas").setAttribute("class", "blurred")
+    Array.from(document.getElementsByClassName("projectLink")).forEach( elem => {
+      if(elem != element){
+        elem.setAttribute("class", "projectLink blurred")
+      }
+      else{
+        elem.setAttribute("class", "projectLink")
+      }
+    })
+  })
+  element.addEventListener('mouseout', mouse => {
+    document.getElementById("myCanvas").setAttribute("class", "notBlurred")
+    Array.from(document.getElementsByClassName("projectLink")).forEach( elem => elem.setAttribute("class", "projectLink notBlurred"))
+  });
+})
+
+window.addEventListener('load', init)
+window.addEventListener('resize', resizeScene)
